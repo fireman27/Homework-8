37d0a54f004e662550b79decf6f4b69d8983d6c6
fireman <fireman2747@yandex.ru>
2022-11-10 12:39:38 +0800

    add Homework 001 произведение двух матриц

---
+++ Homework001/Program.cs
@@ -0,0 +1,73 @@
+﻿// Найти произведение двух матриц
+
+void FillArray(int[,] matr, int[,] matr1)
+{
+    for (int a = 0; a < matr.GetLength(0); a++)
+    {
+        for (int b = 0; b < matr.GetLength(1); b++)
+        {
+            matr[a, b] = new Random().Next(1, 10);
+        }
+    }
+    for (int a = 0; a < matr1.GetLength(0); a++)
+    {
+        for (int b = 0; b < matr1.GetLength(1); b++)
+        {
+            matr1[a, b] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr, int[,] matr1)
+{
+    for (int a = 0; a < matr.GetLength(0); a++)
+    {
+        for (int b = 0; b < matr.GetLength(1); b++)
+        {
+            Console.Write($"{matr[a, b]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    for (int a = 0; a < matr1.GetLength(0); a++)
+    {
+        for (int b = 0; b < matr1.GetLength(1); b++)
+        {
+            Console.Write($"{matr1[a, b]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Composition(int[,] matr, int[,] matr1, int[,] multy)
+{
+    for (int a = 0; a < matr.GetLength(0); a++)
+    {
+        for (int b = 0; b < matr.GetLength(1); b++)
+        {
+            multy[a, b] = matr[a, b] * matr1[a, b];
+        }
+    }
+    
+}
+
+void PrintMultyArray(int[,] multy)
+{
+    for (int a = 0; a < multy.GetLength(0); a++)
+    {
+        for (int b = 0; b < multy.GetLength(1); b++)
+        {
+            Console.Write($"{multy[a, b]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[2, 2];
+int[,] matrix1 = new int[2, 2];
+int[,] multy = new int[2, 2];
+FillArray(matrix, matrix1);
+PrintArray(matrix, matrix1);
+Console.WriteLine();
+Composition(matrix, matrix1, multy);
+PrintMultyArray(multy);
